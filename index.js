955e080beb58314890ec4c288c912fc924606c48
Irfana <irfanairfana23267@gmail.com>
2026-04-06 10:46:08 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("welcome to js world!");
+console.log('welcome to js world!');
+console.log("welcome to js world!");
+
+//
+
+let x;
+let y;
+
+x=20;
+y=30;
+
+console.log(x,y);
+
+let age = 25;
+let price =10.45;
+let 
